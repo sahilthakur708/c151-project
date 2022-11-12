635974a3a37a5c4126694a9a06965036553bb511
sahilthakur708 <70841632+sahilthakur708@users.noreply.github.com>
2022-11-12 10:46:42 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent("car",{
+    schema:{
+        rotateY:{type:'number', default:1}
+    },
+    tick:function(){
+       this.data.rotateY+=0.1
+       var rotation=this.el.getAttribute('rotation')
+       rotation.y=this.data.rotateY
+       this.el.setAttribute('rotation',{x:rotation.x,y:rotation.y,z:rotation.z})
+    }
+})
